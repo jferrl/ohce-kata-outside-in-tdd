365b3e53d84922601cb32249e8760656b350ba30
jferrl <jorgeferrerolinacero@gmail.com>
2020-10-22 00:13:42 +0200

    Implement good morning greeting

--- src/ohce.ts
+++ src/ohce.ts
@@ -1,5 +1,11 @@
+import { Greeter } from './greeter';
+import { Informer } from './informer';
+
 export class Ohce {
-    start(): string {
-        return '¡Buenos días Pepe!';
+    constructor(private readonly greeter: Greeter, private readonly informer: Informer) {}
+
+    run(username: string): void {
+        const greeting = this.greeter.greet(username);
+        this.informer.greetUser(greeting);
     }
 }
